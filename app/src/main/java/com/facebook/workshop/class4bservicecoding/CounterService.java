077f4b667447854dcfc041b8bc37e5881267550d
Kevin Leong <kgleong@gmail.com>
2015-08-07 13:40:22 -0700

    Additional services exercise proposed by Jean Gareau.

---
+++ app/src/main/java/com/facebook/workshop/class4bservicecoding/CounterService.java
@@ -0,0 +1,67 @@
+package com.facebook.workshop.class4bservicecoding;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v4.content.LocalBroadcastManager;
+
+public class CounterService extends Service {
+  public static final String TAG = CounterService.class.getSimpleName();
+  public static final String COUNT = "count";
+  public static final String ACTION = "com.facebook.class4b.go";
+
+  private volatile Looper mServiceLooper;
+  private LocalBroadcastManager mLocalBroadcastManager;
+  private ServiceHandler mServiceHandler;
+  private int mCount;
+  private static final int DELAY_MS = 1000;
+
+  /**
+   * Handler running on a background handler thread.
+   */
+  private final class ServiceHandler extends Handler {
+    public ServiceHandler(Looper looper) {
+      super(looper);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+      Intent intent = new Intent(ACTION);
+      intent.putExtra(COUNT, Integer.toString(++mCount));
+
+      // TODO: broadcast the message to the activity
+      mLocalBroadcastManager.sendBroadcast(intent);
+
+      mServiceHandler.sendEmptyMessageDelayed(0, DELAY_MS);
+    }
+  }
+
+  public void onCreate() {
+    super.onCreate();
+
+    mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+    // An Android handler thread internally operates on a looper.
+    HandlerThread thread = new HandlerThread("IntentService[" + TAG + "]");
+    thread.start();
+    mServiceLooper = thread.getLooper();
+
+    // An Android service handler is a handler running on a specific background thread.
+    mServiceHandler = new ServiceHandler(mServiceLooper);
+    mServiceHandler.sendEmptyMessageDelayed(0, DELAY_MS);
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
+
+  @Override
+  public void onDestroy() {
+    mServiceLooper.quit();
+  }
+}
---
+++ app/src/main/java/com/facebook/workshop/class4bservicecoding/MainActivity.java
@@ -0,0 +1,81 @@
+package com.facebook.workshop.class4bservicecoding;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+  LocalBroadcastManager mLocalBroadcastManager;
+  TextView mCounterTextView;
+
+  /**
+   * This broadcast receiver updates the counter TextView with the
+   * CounterService.COUNT intent char sequence value.
+   */
+  BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      mCounterTextView.setText(intent.getCharSequenceExtra(CounterService.COUNT));
+    }
+  };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+    mCounterTextView = (TextView) findViewById(R.id.counter_textview);
+
+    Button goButton = (Button) findViewById(R.id.go_button);
+
+    goButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // TODO: start the CounterService service. Hint: see newCounterServiceIntent().
+        startService(newCounterServiceIntent());
+      }
+    });
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    // TODO: register the mBroadcastReceiver receiver for CounterService.ACTION.
+    IntentFilter intentFilter = new IntentFilter(CounterService.ACTION);
+    mLocalBroadcastManager.registerReceiver(mBroadcastReceiver, intentFilter);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    // TODO: unregister the mBroadcastReceiver receiver.
+    // Bonus: make sure not to unregister the service if it's already unregistered.
+    mLocalBroadcastManager.unregisterReceiver(mBroadcastReceiver);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    // TODO: stop the service. Bonus: only stop the service if it was started.
+    stopService(newCounterServiceIntent());
+  }
+
+  /**
+   * Make a new intent referring to the CounterService service.
+   */
+  private Intent newCounterServiceIntent() {
+    return new Intent(MainActivity.this, CounterService.class);
+  }
+}
